5f04e4d9b8e47a16350c0bb6f473e76d30e91e38
mater <bxh8640@gmail.com>
2021-07-19 16:49:04 +0800

    fix: dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import ts from 'typescript';
-
-export declare function build(): void;
-
-export declare function createProgram(fileNames: string[], options: ts.CompilerOptions): ts.Program;
-
-export declare function transfromFactory(context: ts.TransformationContext): (sourceFile: ts.SourceFile | ts.Bundle) => ts.SourceFile | ts.Bundle;
-
-export { }
